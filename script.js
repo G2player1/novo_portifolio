6733fc1c4b48b5018041e77565c8836d890c03e8
Enos <enoshenrique008@gmail.com>
2025-05-23 22:32:01 -0300

    modificação de seção resumo e apresentação de certificados: criação de botões para troca de certificado e troca de visualização de pdfs para imagens

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ function troca(id){
     }
     document.getElementById(id).className = "header__nav-link header__nav-link--active";
 }
+
+function trocarCertificado(number) {
+    img = document.getElementById("certificado");
+    download_btn = document.getElementById("download_btn");
+    if(number == 1){
+        img.src = "./assets/certificados/Certificado_LigaJovem_1.jpg";
+        download_btn.href = "./assets/certificados/Certificado_LigaJovem.pdf";
+    } else if (number == 2){
+        img.src = "./assets/certificados/Certificado-OracleNextEducation_1.jpg";
+        download_btn.href = "./assets/certificados/Certificado-OracleNextEducation.pdf";
+    } else if (number == 3){
+        img.src = "./assets/certificados/Certificado_SpringBoot_1.jpg";
+        download_btn.href = "./assets/certificados/Certificado_SpringBoot.pdf";
+    } else if (number == 4){
+        img.src = "./assets/certificados/Certificado_Java_OO_1.jpg";
+        download_btn.href = "./assets/certificados/Certificado_Java_OO.pdf";
+    } else if (number == 5){
+        img.src = "./assets/certificados/Certificado_Git_Github.jpg";
+        download_btn.href = "./assets/certificados/Certificado_Git_Github.pdf";
+    } else if (number == 6){
+        img.src = "./assets/certificados/Certificado_Logica_JavaScript_1.jpg";
+        download_btn.href = "./assets/certificados/Certificado_Logica_JavaScript.pdf";
+    } else if (number == 7){
+        img.src = "./assets/certificados/Certificado_HTML_CSS_1.jpg";
+        download_btn.href = "./assets/certificados/Certificado_HTML_CSS.pdf";
+    }
+    
+}
